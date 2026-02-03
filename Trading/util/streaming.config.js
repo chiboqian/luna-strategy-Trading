1610f1101c73406a9acfad77c7642ad895150f49
Chibo Qian <chiboqian@gmail.com>
2026-02-03 17:48:03 -0500

    bug fix

--- Trading/util/streaming.config.js
+++ Trading/util/streaming.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   apps : [{
-    name: "alpaca-stream-framework",
+    name: "streaming",
     script: "./Trading/stream_framework.py",
     interpreter: "./.venv/bin/python", // Points to your venv
     cwd: "/home/paper1/luna-strategy-Trading/Trading", // Ensure this path is correct
