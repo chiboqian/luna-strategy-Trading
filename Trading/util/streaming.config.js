82379cae193f79c61c3c4a75d4f39c8625276f4e
Chibo Qian <chiboqian@gmail.com>
2026-02-05 10:53:41 -0500

    updated pm2 service file

--- Trading/util/streaming.config.js
+++ Trading/util/streaming.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   apps : [{
     name: "streaming",
-    script: "./Trading/stream_framework.py --config ./config/streaming_rules/",
+    script: "./Trading/stream_framework.py",
+    args: "--config ./config/streaming_rules/",
     interpreter: "./.venv/bin/python", // Points to your venv
-    cwd: "/home/paper1/luna-strategy-Trading/Trading", // Ensure this path is correct
+    cwd: "~/paper1/luna-strategy-Trading/Trading", // Ensure this path is correct
     autorestart: true,
     watch: true,
+    ignore_watch: ["trading_logs", "logs", "*.log", "__pycache__", "*.csv"],
     cron_restart: "0 9 * * *",
   }]
 };
